8ec7e65075486198f72a9125584f6dd619a56bad
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-07-07 15:35:32 -0500

    first updates to css

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 
-var questionBox = document.getElementById("questionBox") //links my text area to hold questions
-var timeEl = document.querySelector(".time"); //from timer assignment
-var buttons = document.getElementById("buttons");
-var startBtn = document.getElementById("startBtn")//links my start button
-
-var score = [];
+var questionBox = document.getElementById("questionBox") //link to text area which hold questions from html
+var timeEl = document.querySelector(".time"); //link to time text in html
+var buttons = document.getElementById("buttons");//link to section in html to contain generated buttons
+var startBtn = document.getElementById("startBtn");//link to start button from html
 var correct = 0;
-
 var questionIndex = 0; //setting the index of my questions to start at 0 (the first object in the array)
 var answerIndex = 0; //setting the index of my answer to start at 0 (the first line within the array)
-startBtn.addEventListener("click", beginQuiz);//event listener on the start button - should declare my function to begin quiz
-
+startBtn.addEventListener("click", beginQuiz);
+//function used to advance to next question
 function advance () {  
   clearContent();
   genButton();
   buttons.style.display = "block";
   showQuestion();
 }
-
+//function to clear generated buttons
 function clearContent() {
   document.getElementById('buttons').innerHTML = "";
-}
-    
-//created an array with all of my questions, each of which contain an array of answers and their "correctness"
-
+}  
+//here is an array with all of the questions, each of which contain an array of answers and their "correctness"
 let questions = [
   {
     question: 'What does HTML stand for?',
@@ -62,7 +57,7 @@ let questions = [
     ]
   }
 ];
-  
+//function to control timer  
 var secondsLeft = 40;
 function setTime() {
   var timerInterval = setInterval(function() {
@@ -74,24 +69,31 @@ function setTime() {
   }
   }, 1000);
 }  
-
+//function to begin quiz 
+function beginQuiz() {
+  startBtn.style.display = "none";  
+  questionBox.style.display = "block";
+  setTime()
+  genButton();
+  showQuestion();
+}
+//function to show questions
 var lastQuestion = questions.length;
 function showQuestion() {
-  if(questionIndex === lastQuestion) { //this may not be necessary - see if you can fix error
+  if(questionIndex === lastQuestion) {
   } 
   else {
+  questionBox.innerHTML = questions[questionIndex].question;
   questionIndex++;
   answerIndex++;
-  questionBox.innerHTML = questions[questionIndex].question;
+ 
   } 
 }
-
+//function to generate buttons containing answers from object within array of questions
 function genButton() {
   if(questionIndex === questions.length){
     endQuiz();}
   else {
-    console.log(answerIndex)
-    console.log(questions[questionIndex].answers.length)
     for(i = 0; i < questions[questionIndex].answers.length; i++){
       var button = document.createElement('button');
       button.dataset.correct = questions[questionIndex].answers[i].correct;
@@ -102,12 +104,11 @@ function genButton() {
     }
   }
 }
- 
+//function to track user's score based on answers chosen and reduce time for incorrect answers
 function userAnswer(event) {
   const selectedButton = event.target;
   if (selectedButton.dataset.correct === 'true') {
   correct++;
-  
   buttons.style.display = "none";
   advance();
   }
@@ -117,29 +118,7 @@ function userAnswer(event) {
   advance();
   }
 }
-
-function beginQuiz() {
-  startBtn.style.display = "none";  
-  questionBox.style.display = "block";
-  setTime()
-  genButton();
-  showQuestion();
-}
-function tryAgain() {
-  secondsLeft = 40;
-  questionIndex = 0;
-  answerIndex = 0;
-  correct = 0;
-  buttons.innerHTML = "";
-  beginQuiz();
-}
-
-function prevScore() {
-  document.getElementById(scoreboard);
-  var userIni = localStorage.getItem("userIni");
-  scoreboard.innerHTML = userIni;
-}
-
+//function to end quiz and allow user to submit score
 function endQuiz() {
   secondsLeft = 1;
   var userIni = document.createElement('input');
@@ -152,14 +131,12 @@ function endQuiz() {
   buttons.append(submitBtn);
   userIni.setAttribute('id', "userIni");
   submitBtn.addEventListener("click", function(event) {
-  if(userIni.value === "") {
-    alert("don't forget to enter your name")
+  if(userIni.value === "") { //keeps user from entering null for scoreboard
+    alert("Don't forget to enter your initials")
   }
-  else {
+  else {//stores user's initials, hides submit button and reveals try again button
     event.preventDefault();
     var initials = document.getElementById('userIni').value;
-    console.log(document.getElementById('userIni').value)
-    console.log(userIni.style.display)
     userIni.style.display = "none";
     localStorage.setItem("userIni", initials + " : " + correct +"/"+ questions.length)
     prevScore() 
@@ -169,13 +146,26 @@ function endQuiz() {
   }); 
   var restartBtn = document.createElement('button');
   restartBtn.style.display = "none";
+  restartBtn.setAttribute('id','center')
   restartBtn.innerText = "Try again?";
   buttons.append(restartBtn);
   restartBtn.addEventListener("click", tryAgain);
 } 
-
-
-
+//function for "try again" button which resets variables needed and allows user to restart the quiz
+function tryAgain() {
+  secondsLeft = 40;
+  questionIndex = 0;
+  answerIndex = 0;
+  correct = 0;
+  buttons.innerHTML = "";
+  beginQuiz();
+}
+//function that displays the most recent user's entry and score
+function prevScore() {
+  document.getElementById(scoreboard);
+  var userIni = localStorage.getItem("userIni");
+  scoreboard.innerHTML = userIni;
+}
 
 
   
